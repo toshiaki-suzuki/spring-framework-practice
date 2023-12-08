22f156b0f9a9db0f3227696d39db32e8932de0be
toshiaki-suzuki <saki0x4bf@gmail.com>
2023-12-08 19:45:57 +0900

    Refactor SampleAspect to use @Around advice

--- DependencyInjectionSample/src/main/java/com/example/demo/chapter03/aop/SampleAspect.java
+++ DependencyInjectionSample/src/main/java/com/example/demo/chapter03/aop/SampleAspect.java
@@ -1,27 +1,20 @@
 package com.example.demo.chapter03.aop;
 
-import java.text.SimpleDateFormat;
-
-import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class SampleAspect {
-	@Before("execution(* com.example.demo.chapter03.used.Greet.*(..))")
-	public void beforeAdvice(JoinPoint joinpoint) {
-		System.out.println("===Before Advice===");
-		System.out.println(new SimpleDateFormat("yyyy/MM/dd").format(new java.util.Date()));
-		System.out.println(String.format("メソッド:%s", joinpoint.getSignature().getName()));
-	}
-	
-	@After("execution(* com.example.demo.chapter03.used.Greet.*(..))")
-	public void afterAdvice(JoinPoint joinpoint) {
-		System.out.println("===After Advice===");
-		System.out.println(new SimpleDateFormat("yyyy/MM/dd").format(new java.util.Date()));
-		System.out.println(String.format("メソッド:%s", joinpoint.getSignature().getName()));
+	@Around("execution(* com.example.demo.chapter03.used.Greet.*(..))")
+	public Object afterAdvice(ProceedingJoinPoint joinPoint) throws Throwable {
+		System.out.println("===Around Advice===");
+		System.out.println("▼▼▼処理前▼▼▼");
+		Object result = joinPoint.proceed();
+		System.out.println("▲▲▲処理後▲▲▲");
+		
+		return result;
 	}
 }
