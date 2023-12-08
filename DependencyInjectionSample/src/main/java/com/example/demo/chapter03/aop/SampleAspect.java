f5bb2b8c9d9977e4f799a988d7a8887b0e2ed4d5
toshiaki-suzuki <saki0x4bf@gmail.com>
2023-12-08 19:38:12 +0900

    Add After Advice to SampleAspect

--- DependencyInjectionSample/src/main/java/com/example/demo/chapter03/aop/SampleAspect.java
+++ DependencyInjectionSample/src/main/java/com/example/demo/chapter03/aop/SampleAspect.java
@@ -3,6 +3,7 @@ package com.example.demo.chapter03.aop;
 import java.text.SimpleDateFormat;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -13,6 +14,13 @@ public class SampleAspect {
 	@Before("execution(* com.example.demo.chapter03.used.Greet.*(..))")
 	public void beforeAdvice(JoinPoint joinpoint) {
 		System.out.println("===Before Advice===");
+		System.out.println(new SimpleDateFormat("yyyy/MM/dd").format(new java.util.Date()));
+		System.out.println(String.format("メソッド:%s", joinpoint.getSignature().getName()));
+	}
+	
+	@After("execution(* com.example.demo.chapter03.used.Greet.*(..))")
+	public void afterAdvice(JoinPoint joinpoint) {
+		System.out.println("===After Advice===");
 		System.out.println(new SimpleDateFormat("yyyy/MM/dd").format(new java.util.Date()));
 		System.out.println(String.format("メソッド:%s", joinpoint.getSignature().getName()));
 	}
