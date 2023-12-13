d2c0b712641531952e99b71a29b3b02bb4b026e0
toshiaki-suzuki <saki0x4bf@gmail.com>
2023-12-13 20:39:40 +0900

    Add member insertion and selection functionality

--- SpringDataJDBCSample/src/main/java/com/example/demo/SpringDataJdbcSampleApplication.java
+++ SpringDataJDBCSample/src/main/java/com/example/demo/SpringDataJdbcSampleApplication.java
@@ -1,13 +1,41 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.demo.entity.Member;
+import com.example.demo.repository.MemberCrudRepository;
 
 @SpringBootApplication
 public class SpringDataJdbcSampleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringDataJdbcSampleApplication.class, args);
+		SpringApplication.run(SpringDataJdbcSampleApplication.class, args)
+		.getBean(SpringDataJdbcSampleApplication.class)
+		.execute();
+	}
+	
+	@Autowired
+	MemberCrudRepository repository;
+	
+	private void execute() {
+		executeInsert();
+		executeSelect();
+	}
+	
+	private void executeInsert() {
+		Member member = new Member(null, "花子");
+		repository.save(member);
+		System.out.println("登録したデータ:" + member);
+	}
+	
+	private void executeSelect() {
+		System.out.println("---全件取得します---");
+		Iterable<Member> members = repository.findAllById(null);
+		for(Member member: members) {
+			System.out.println(member);
+		}
 	}
 
 }
