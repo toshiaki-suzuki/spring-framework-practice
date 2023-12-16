b80bd9becc91585078d66a6e3cca290d633205e7
toshiaki-suzuki <saki0x4bf@gmail.com>
2023-12-16 11:31:50 +0900

    Add Member entity and update ThymeleafController and useThymeleaf.html

--- ThymeleafSample/src/main/java/com/example/demo/controller/ThymeleafController.java
+++ ThymeleafSample/src/main/java/com/example/demo/controller/ThymeleafController.java
@@ -4,13 +4,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.demo.entity.Member;
+
 @Controller
 public class ThymeleafController {
 
-	@GetMapping("show") // /show へのリクエストを処理
+	@GetMapping("show")
 	public String showView(Model model) {
-		// 戻り値は「ビュー名」を返す
+		// Memberを作成
+		Member member = new Member(1, "会員01");
+		// 「Model」にデータを格納する
 		model.addAttribute("name", "太郎");
+		model.addAttribute("mb", member);
+		// 戻り値は「ビュー名」を返す
 		return "useThymeleaf";
 	}
 }
