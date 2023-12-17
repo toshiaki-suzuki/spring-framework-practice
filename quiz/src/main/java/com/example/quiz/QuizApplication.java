e21ee5c118f80482d02c84f950c8413860dfbd67
toshiaki-suzuki <saki0x4bf@gmail.com>
2023-12-17 16:06:29 +0900

    Add updateQuiz() method to QuizApplication.java

--- quiz/src/main/java/com/example/quiz/QuizApplication.java
+++ quiz/src/main/java/com/example/quiz/QuizApplication.java
@@ -26,7 +26,9 @@ public class QuizApplication {
 		// 全件取得
 //		showList();
 		// 一件取得
-		showOne();
+//		showOne();
+		// 更新処理
+		updateQuiz();
 	}
 	/** === クイズを2件登録します === */
 	private void setup() {
@@ -66,5 +68,16 @@ public class QuizApplication {
 			System.out.println("該当する問題が存在しません・・・");
 		}
 		System.out.println("--- １件取得完了 ---");
+	}
+	/** === 更新処理 === */
+	private void updateQuiz() {
+		System.out.println("--- 更新処理開始 ---");
+		// 変更したいエンティティを生成する
+		Quiz quiz1 = new Quiz(1, "「スプリング」はフレームワークですか？", true, "変更タロウ");
+		// 更新実行
+		quiz1 = repository.save(quiz1);
+		// 更新確認
+		System.out.println("更新したデータは、" + quiz1 + "です。");
+		System.out.println("--- 更新処理完了 ---");
 	}
 }
