41ff34f34518d982ee126437f9597322535af015
toshiaki-suzuki <saki0x4bf@gmail.com>
2023-12-23 15:32:48 +0900

    Add showUpdate method to QuizController

--- quiz/src/main/java/com/example/quiz/controller/QuizController.java
+++ quiz/src/main/java/com/example/quiz/controller/QuizController.java
@@ -1,5 +1,7 @@
 package com.example.quiz.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,6 +9,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -65,5 +68,72 @@ public class QuizController {
 			// エラーがある場合は、一覧表示処理を呼びます
 			return showList(quizForm, model);
 		}
+	}
+	/** Quizデータを１件取得し、フォーム内に表示する */
+	@GetMapping("/{id}")
+	public String showUpdate(
+			QuizForm quizForm,
+			@PathVariable Integer id, // パスパラメータ
+			Model model) {
+		//Quizを取得(Optionalでラップ)
+		Optional<Quiz> quizOpt = service.selectOneById(id);
+		//QuizFormへの詰め直し
+		Optional<QuizForm> quizFormOpt = quizOpt.map(t -> makeQuizForm(t));
+		//QuizFormがnullでなければ中身を取り出す
+		if (quizFormOpt.isPresent()) {
+			quizForm = quizFormOpt.get();
+		}
+		// 更新用のModelを作成する
+		makeUpdateModel(quizForm, model);
+		return "crud";
+	}
+	/** 更新用のModelを作成する */
+	private void makeUpdateModel(QuizForm quizForm, Model model) {
+		model.addAttribute("id", quizForm.getId());
+		quizForm.setNewQuiz(false);
+		model.addAttribute("quizForm", quizForm);
+		model.addAttribute("title", "更新用フォーム");
+	}
+	/** idをKeyにしてデータを更新する */
+	@PostMapping("/update")
+	public String update(
+			@Validated QuizForm quizForm,
+			BindingResult result,
+			Model model,
+			RedirectAttributes redirectAttributes) {
+		//QuizFormからQuizに詰め直す
+		Quiz quiz = makeQuiz(quizForm);
+		// 入力チェック
+		if (!result.hasErrors()) {
+			//更新処理、フラッシュスコープの使用、リダイレクト（個々の編集ページ）
+			service.updateQuiz(quiz);
+			redirectAttributes.addFlashAttribute("complete", "更新が完了しました");
+			// 更新画面を表示する
+			return "redirect:/quiz/" + quiz.getId();
+		} else {
+			// 更新用のModelを作成する
+			makeUpdateModel(quizForm, model);
+			return "crud";
+		}
+	}
+	// ---------- 【以下はFormとDomainObjectの詰めなおし】 ----------
+	/** QuizFormからQuizに詰め直して戻り値とし返します */
+	private Quiz makeQuiz(QuizForm quizForm) {
+		Quiz quiz = new Quiz();
+		quiz.setId(quizForm.getId());
+		quiz.setQuestion(quizForm.getQuestion());
+		quiz.setAnswer(quizForm.getAnswer());
+		quiz.setAuthor(quizForm.getAuthor());
+		return quiz;
+	}
+	/** QuizからQuizFormに詰め直して戻り値とし返します */
+	private QuizForm makeQuizForm(Quiz quiz) {
+		QuizForm form = new QuizForm();
+		form.setId(quiz.getId());
+		form.setQuestion(quiz.getQuestion());
+		form.setAnswer(quiz.getAnswer());
+		form.setAuthor(quiz.getAuthor());
+		form.setNewQuiz(false);
+		return form;
 	}
 }
